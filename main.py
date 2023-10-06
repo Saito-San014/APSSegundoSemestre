1fa18af5d637028cd30c7314f3aaf059a3e044da
Saito-San014 <gabi.loure1@outlook.com>
2023-10-05 21:45:17 -0300

    rsa first test

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print("APS Hello World!")#aoba
+#pip install rsa
+#Precisa excutar o comando acima no terminal para o codigo rodar
+import rsa
+
+#Cria as chaves publicas e privadas
+publicKey, privateKey = rsa.newkeys(512)
+text = input("Digite o texto: \n")
+
+#Texto sendo criptografado com a chave publica
+encodeText = rsa.encrypt(text.encode(), publicKey)          
+#Desncripta o texto
+decodeText = rsa.decrypt(encodeText, privateKey).decode()
+
+print(f" Texto original: {text} Tamanho: {len(text)}")
+print(f" Chave publica: {publicKey} \n Tamanho: {len(str(publicKey))}")
+print(f" Chave privada: {privateKey} \n Tamanho: {len(str(privateKey))}")
+print(f" Texto Criptogafado: {encodeText} Tamanho: {len(encodeText)}")
+print(f" Texto original: {decodeText} Tamanho: {len(decodeText)}")
